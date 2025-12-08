c15d18c71c50e38169a4399f91130d31f2de935d
ZxiEcho <jacob.enbjarde@gmail.com>
2025-12-08 11:05:00 +0100

    dag 3 del 1 klar

--- src/day3.rs
+++ src/day3.rs
@@ -1,7 +1,7 @@
 use std::{fs};
 
 pub fn day3_p1() -> i32{
-    let input = fs::read_to_string("day3.txt").expect("error input");
+    let input = fs::read_to_string("test.txt").expect("error input");
 
     let result = input.lines().map(|v|{
         let mut big_left = 0;
@@ -12,7 +12,7 @@ pub fn day3_p1() -> i32{
         let mut reserv_id = v.len();
         let line = v.as_bytes();
         let mut j = v.len(); 
-        for i in 0..v.len(){
+        for i in 0..v.len() - 1{
             j -= 1;
             if line[i] > big_left && i < index_right{
                 big_left = line[i];
@@ -21,8 +21,10 @@ pub fn day3_p1() -> i32{
             }
             if line[j] > big_right && j > index_left{
                 reserv = big_right;
+                //println!("{}", reserv as i32 - b'0' as i32);
                 reserv_id = index_right;
                 big_right = line[j];
+                //println!("{}", big_right as i32 - b'0' as i32);
                 index_right = j;
             }
             if line[i] > big_left && big_right > big_left && i == index_right{
@@ -30,10 +32,12 @@ pub fn day3_p1() -> i32{
                 index_left = i;
                 big_right = reserv;
                 index_right = reserv_id;
+                
             }
         }
+        println!("{} {}",big_left as i32 - b'0' as i32, big_right as i32 - b'0' as i32 );
 
-        (big_left - b'0') as i32 * 10 + (big_right - b'0') as i32
+        (big_left as i32 - b'0' as i32) * 10 + (big_right as i32 - b'0' as i32)
         
     }).sum();
 
